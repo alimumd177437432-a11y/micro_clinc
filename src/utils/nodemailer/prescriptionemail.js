faa9fafbb19de364ee88415339160e05c9ff8055
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-06-06 10:57:53 +0300

    update the new resend

--- src/utils/nodemailer/prescriptionemail.js
+++ src/utils/nodemailer/prescriptionemail.js
@@ -1,15 +1,23 @@
-import { Resend } from "resend";
-import { prescriptionTemplate } from "./prescriptiontemplate.js";
-
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
+import { prescriptionTemplate } from "./prescriptiontemplate";
 
 export const sendPrescriptionEmail = async ({ patientEmail, patientName, doctorName, diagnosis, medications, appointmentDate }) => {
+  
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4, // ✅ إجبار IPv4 عشان يشتغل على Render
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
+    },
+  });
 
   const html = prescriptionTemplate({ patientName, doctorName, diagnosis, medications, appointmentDate });
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  await transporter.sendMail({
+    from: `"عيادة الشفاء الرقمية 🏥" <${process.env.NODEMAILER_USER}>`,
     to: patientEmail,
     subject: `روشتتك الطبية من د. ${doctorName} 💊`,
     html,
