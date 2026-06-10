d65c9eb8fe35181713fbfc8cd4ac2f7daf8eee44
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-06-10 16:57:55 +0300

    update the doctor routes

--- src/modules/routes/doctor_routes.js
+++ src/modules/routes/doctor_routes.js
@@ -3,7 +3,6 @@ import { authentication, authoriziation } from "../../middelwares.js/auth_middel
 import { getMyProfile, updateDoctorProfile } from "../controler/doctor_controler.js";
 import { updateDoctorSchema } from "../../middelwares.js/schemas.js";
 import { validate } from "../../middelwares.js/validate.js";
-import { getMyAppointments } from "../controler/patient_controler.js";
 
 const doctorRouter = Router();
 doctorRouter.use(authentication, authoriziation("doctor"));
@@ -50,6 +49,15 @@ doctorRouter.put("/", validate(updateDoctorSchema), updateDoctorProfile);
  *         description: بروفايل الدكتور مع السلوتس
  */
 doctorRouter.get("/profile", getMyProfile);
-doctorRouter.get("/appointments", authoriziation("doctor"), getMyAppointments);
+// جلب كل مواعيد الدكتور
+doctorRouter.get("/appointments", authentication, authoriziation("doctor"), async (req, res) => {
+  const doctorId = req.user.id;
+  const appointments = await appointmentModel
+    .find({ doctorId })
+    .populate("patientId", "name email");
+  
+  res.json({ status: "success", data: { appointments } });
+});
+
 
 export default doctorRouter;
