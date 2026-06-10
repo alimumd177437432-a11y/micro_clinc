f5e6b3cf3f4617f5a387a510984e9107f3b544c8
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-06-10 16:59:46 +0300

    update the doctor routes

--- src/modules/routes/doctor_routes.js
+++ src/modules/routes/doctor_routes.js
@@ -3,6 +3,7 @@ import { authentication, authoriziation } from "../../middelwares.js/auth_middel
 import { getMyProfile, updateDoctorProfile } from "../controler/doctor_controler.js";
 import { updateDoctorSchema } from "../../middelwares.js/schemas.js";
 import { validate } from "../../middelwares.js/validate.js";
+import { appointmentModel } from "../models/Appointment_model.js";
 
 const doctorRouter = Router();
 doctorRouter.use(authentication, authoriziation("doctor"));
